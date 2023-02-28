698a26ab19229027fff280e91de1562c2b3a9a62
0lzi <oliver.kelly@thehutgroup.com>
2023-02-28 12:11:07 +0000

    Create update.sh

---
+++ nextcloud/update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#Script to update nextcloud LSIO Image and Nextcloud Version
+
+echo "Pulling New LSIO images"
+docker-compose pull
+
+echo "Complete, updating containers"
+docker-compose up -d
+
+echo "Containers running, pruning old images"
+docker image prune
+
+wait 100
+echo "Updating Nextcloud to newest version"
+
+docker exec -it nextcloud updater.phar
+
+exit
+echo "Update Complete"
